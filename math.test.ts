0008e1cdcec40181de0d20d293258a44783674cb
V-Leksa <157983588+V-Leksa@users.noreply.github.com>
2025-05-05 16:28:13 +0200

    Update math.test.ts
    correction

--- math.test.ts
+++ math.test.ts
@@ -1,5 +1,5 @@
-function returnAddComponents (a, b) {
-    return a + b;
-}
+const {returnAddComponents} = require ('./math');
 
-module.exports = {returnAddComponents};
+test ('adds 2 + 3 to equal 5', () => {
+    expect (returnAddComponents (2, 3).toBe (5));
+});
