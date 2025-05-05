89b33e3eb8be5a363db759fa34405ee537ab6a21
Александра <smit88-8@mail.ru>
2025-05-05 16:12:17 +0200

    update

--- math.js
+++ math.js
@@ -1,1 +1,5 @@
+function returnDivideComponents (a, b) {
+    return a / b;
+}
 
+module.exports = {returnDivideComponents};
--- math.test.ts
+++ math.test.ts
@@ -1,1 +1,5 @@
+const {returnDivideComponents} = require ('./math');
 
+test ('adds 10 / 5 to equal 2', () => {
+    expect (returnDivideComponents (10, 5).toBe (2));
+});
