7dcf59a566b6a052f7fad987479c5ea97d36a0ba
Александра <smit88-8@mail.ru>
2025-05-05 15:09:00 +0200

    correction

--- math.js
+++ math.js
@@ -1,5 +1,5 @@
-function returnDivideComponents (a, b) {
-    return a / b;
+function returnAddComponents (a, b) {
+    return a + b;
 }
 
-module.exports = {returnDivideComponents};
+module.exports = {returnAddComponents};
--- math.test.ts
+++ math.test.ts
@@ -1,5 +1,5 @@
-const {returnDivideComponents} = require ('./math');
+const {returnAddComponents} = require ('./math');
 
-test ('adds 10 / 5 to equal 2', () => {
-    expect (returnDivideComponents (10, 5).toBe (2));
+test ('adds 2 + 3 to equal 5', () => {
+    expect (returnAddComponents (2, 3).toBe (5));
 });
