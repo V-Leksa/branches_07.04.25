7fc5482be279cbdc69ea13faf92b7b29ceda3f93
Александра <smit88-8@mail.ru>
2025-05-05 14:50:41 +0200

    add function on brunch addComponents

--- math.js
+++ math.js
@@ -1,5 +1,5 @@
-function returnAddComponents (a, b) {
-    return a + b;
+function returnDivideComponents (a, b) {
+    return a / b;
 }
 
-module.exports = {returnAddComponents};
+module.exports = {returnDivideComponents};
--- math.test.ts
+++ math.test.ts
@@ -1,5 +1,5 @@
-const {returnAddComponents} = require ('./math');
+const {returnDivideComponents} = require ('./math');
 
-test ('adds 2 + 3 to equal 5', () => {
-    expect (returnAddComponents (2, 3).toBe (5));
+test ('adds 10 / 5 to equal 2', () => {
+    expect (returnDivideComponents (10, 5).toBe (2));
 });
